885dab3dedc8596407b2f8b485972bdcaa0c5dd2
fnomadv <fnomad7@gmail.com>
2019-02-11 19:37:35 -0500

    Add files via upload

---
+++ canvas/dibujo.js
@@ -0,0 +1,26 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi,yf,xi,xf;
+
+
+while (l<lineas)
+{
+  yi = 10 * l;
+  xf = 10 * (l +1);
+  dibujarLinea("#AAF",0,yi,xf,300);
+  console.log("lineas " + l);
+  l = l +1;
+}
+
+
+function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial,yinicial);
+  lienzo.lineTo(xfinal,yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
---
+++ dom/codigo.js
@@ -0,0 +1,4 @@
+var ruta = window.location;
+console.log(navigator);
+document.write("Hola estudiante");
+document.write("Estás en: " + ruta);
