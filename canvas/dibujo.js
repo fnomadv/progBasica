fcb69e99bba7afa59cd1860932f95cf05257914e
fnomadv <fnomad7@gmail.com>
2019-02-11 23:01:49 -0500

    Add files via upload

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -3,16 +3,20 @@ var lienzo = d.getContext("2d");
 var lineas = 30;
 var l = 0;
 var yi,yf,xi,xf;
+var colorcito = "#FAA";
 
 
-while (l<lineas)
+for (l = 0;l < lineas; l++)
 {
   yi = 10 * l;
-  xf = 10 * (l +1);
-  dibujarLinea("#AAF",0,yi,xf,300);
+  xf = 10 * (l + 1);
+  dibujarLinea(colorcito,0,yi,xf,300);
   console.log("lineas " + l);
   l = l +1;
 }
+
+dibujarLinea(colorcito,1,1,1,299);
+dibujarLinea(colorcito,1,299,299,299);
 
 
 function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal)
