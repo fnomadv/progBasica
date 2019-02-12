c354374630965424a83c0e72ffef7755f61a548b
fnomadv <fnomad7@gmail.com>
2019-02-11 23:55:37 -0500

    Add files via upload

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,23 +1,34 @@
+var texto = document.getElementById("txtLineas");
+var boton = document.getElementById("btnEnviar");
+boton.addEventListener("click",dibujoPorClic);
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi,yf,xi,xf;
-var colorcito = "#FAA";
 
 
-for (l = 0;l < lineas; l++)
+function dibujoPorClic()
 {
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea(colorcito,0,yi,xf,300);
-  console.log("lineas " + l);
-  l = l +1;
+  console.log(lienzo);
+  var lineas = parseInt(texto.value);
+  var espacio = ancho / lineas;
+  var l = 0;
+  var yi,yf,xi,xf;
+  var colorcito = "#FAA";
+
+
+  for (l = 0;l < lineas; l++)
+  {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(colorcito,0,yi,xf,300);
+    console.log("lineas " + l);
+    l = l +1;
+  }
+
+  dibujarLinea(colorcito,1,1,1,299);
+  dibujarLinea(colorcito,1,299,299,299);
 }
-
-dibujarLinea(colorcito,1,1,1,299);
-dibujarLinea(colorcito,1,299,299,299);
-
 
 function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal)
 {
